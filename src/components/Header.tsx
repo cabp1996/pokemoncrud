75e32539ab45c433529931707d4077e517fdb8db
AndresBalcazar <andres.balcazar@devsu.com>
2022-06-05 17:08:56 -0500

    fix: fix search input style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,17 +23,6 @@ export const Header = () => {
       <h1>Listado de Pokemon</h1>
       <div className="search-bar-container">
         <div className="search-input">
-          <svg
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            stroke="grey"
-            strokeWidth="1"
-            fill="none"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-          </svg>
           <input
             className="form-input"
             placeholder="Buscar"
